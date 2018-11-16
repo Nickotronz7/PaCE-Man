37e244ea0eb9277ced5cb2b9590ee17e2809066e
Nicko <nickolasrc7@gmail.com>
2018-11-16 11:04:11 -0600

    Arreglados los desmadres de dago

---
+++ main.c
@@ -0,0 +1,13 @@
+#include "Game/Headers/game.h"
+#include "Server/Headers/server.h"
+#include "JSON/Headers/json_builder.h"
+
+
+int main() {
+
+//    struct state game = game_core();
+
+    server(8080);
+
+    return 0;
+}
