c11a47d65e097ea193fe19e2023fea67d9e6eb27
Nicko <nickolasrc7@gmail.com>
2018-11-22 04:16:02 -0600

    Interfaz del administrador del juego

---
+++ adimnPacMan/cmake-build-debug/CMakeFiles/adimnPacMan.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/adimnPacMan.dir/main.c.o"
+  "CMakeFiles/adimnPacMan.dir/client.c.o"
+  "CMakeFiles/adimnPacMan.dir/cJSON.c.o"
+  "CMakeFiles/adimnPacMan.dir/cJSON_Utils.c.o"
+  "CMakeFiles/adimnPacMan.dir/json_builder.c.o"
+  "adimnPacMan.pdb"
+  "adimnPacMan"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/adimnPacMan.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
